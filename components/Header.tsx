94c4d05476b29e8d8ea06a132c752c800e7d29bf
Tri Nguyen <tri09t4@gmail.com>
2022-06-28 16:48:50 +0700

    init

---
+++ components/Header.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Header = () => {
+  return (
+    <header>
+      {/* Top nav */}
+      <div>
+        <div>
+          <Image
+            src='https://links.papareact.com/f90'
+            width={150}
+            height={40}
+          />
+        </div>
+      </div>
+
+      {/* Button nav */}
+
+    </header>
+  )
+}
+
+export default Header
