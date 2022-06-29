01764d1e564156387728db5d8ccf41026a37d1b7
Tri Nguyen <tri09t4@gmail.com>
2022-06-29 09:58:24 +0700

    Buttom nav

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-import { SearchIcon, ShoppingCartIcon } from '@heroicons/react/outline'
+import { MenuIcon, SearchIcon, ShoppingCartIcon } from '@heroicons/react/outline'
 
 const Header = () => {
   return (
@@ -47,9 +47,22 @@ const Header = () => {
         </div>
       </div>
 
-
-      {/* Button nav */}
-
+      {/* Buttom nav */}
+      <div className='flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm'>
+        <p className='link flex items-center '>
+          <MenuIcon className='h-6 mr-1'/>
+          All
+        </p>
+        <p className='link'>Prime Video</p>
+        <p className='link'>Amazon Business</p>
+        <p className='link'>Today's Deal</p>
+        <p className='link hidden lg:!inline-flex'>Electronics</p>
+        <p className='link hidden lg:!inline-flex'>Food & Grocery</p>
+        <p className='link hidden lg:!inline-flex'>Prime</p>
+        <p className='link hidden lg:!inline-flex'>Buy Again</p>
+        <p className='link hidden lg:!inline-flex'>Shopper Toolkit</p>
+        <p className='link hidden lg:!inline-flex'>Health & Personal Care</p>
+      </div>
     </header>
   )
 }
