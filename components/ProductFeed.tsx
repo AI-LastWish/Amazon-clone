0eff94c291a3f0950f7cde3898752a961ede3b8a
Tri Nguyen <tri09t4@gmail.com>
2022-07-05 16:23:34 +0700

    use some css trick

--- components/ProductFeed.tsx
+++ components/ProductFeed.tsx
@@ -9,7 +9,23 @@ interface Props {
 const ProductFeed = ({ products }: Props) => {
   return (
     <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'>
-      {products.map(product => (
+      {products.slice(0, 4).map(product => (
+        <Product key={product.id} product={product} />
+      ))}
+
+      <img
+        className='md:col-span-full'
+        src='https://links.papareact.com/dyz'
+        alt='discover'
+      />
+
+      <div className='md:col-span-2'>
+        {products.slice(4, 5).map(product => (
+          <Product key={product.id} product={product} />
+        ))}
+      </div>
+
+      {products.slice(5).map(product => (
         <Product key={product.id} product={product} />
       ))}
     </div>
